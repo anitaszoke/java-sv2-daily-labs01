2688d1fad6a8d39a9c10428176d795d6128519fd
anitaszoke <szanita1735@gmail.com>
2021-10-17 15:28:39 +0200

    day04-task-done

---
+++ day04/src/main/java/week01day04/Hello.java
@@ -0,0 +1,7 @@
+package week01day04;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Ne ess pánikba! És csütörtökön legyen nálad törülköző!");
+    }
+}
